004bd309b365174064f7a018967291c0a94be8ec
Ivan A. Kudryavtsev <kudryavtsev_ia@bw-sw.com>
2022-04-13 14:08:24 +0400

    Updated to a new protocol.

--- src/utils.rs
+++ src/utils.rs
@@ -3,6 +3,6 @@ use std::fs;
 
 
 pub fn load_file(prefix: &Path, schema_name: &str) -> String {
-    let path = format!("insight.edge.{}.avsc", schema_name);
+    let path = format!("insight.transport.{}.avsc", schema_name);
     fs::read_to_string(prefix.join(path)).unwrap()
 }
